61cccd48e9524efe7916d142d209567cdf9b5088
rauni5 <127040514+rauni5@users.noreply.github.com>
2025-05-08 12:16:55 +0545

    ?

--- resources/Java/Comment.js
+++ resources/Java/Comment.js
@@ -1,5 +1,6 @@
 function Addcomment(event){
 event.preventDefault();
+document.getElementById("commment").value;
 document.getElementById("commentsec").innerHTML+=`<div class="team_container2"style="margin-top: 30px;margin-left:120px;">
                     <div class="team_card">
                       <div class="team_img2"><img src="../resources/Imgs/Indivudal blog/Comment 2.jpg"style="border-radius: 100%;"></div>
